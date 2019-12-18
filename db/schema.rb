463327603bc8ef520a9f5a9d7d7662a58962d87e
Sim-Chen <chenzhendoit@163.com>
2019-12-18 03:48:36 -0800

    add cloumns to order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191218085537) do
+ActiveRecord::Schema.define(version: 20191218092730) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20191218085537) do
     t.boolean  "is_paid",          default: false
     t.string   "payment_method"
     t.string   "aasm_state",       default: "order_placed"
+    t.string   "shipping_phone"
     t.index ["aasm_state"], name: "index_orders_on_aasm_state"
   end
 
